c36384fc4b991ab209208b2f3e2f3c70a434137b
Denis Lima <denislima99@gmail.com>
2021-01-22 21:08:08 -0300

    Tratando o botão 'ponto'

--- js/calculate.js
+++ js/calculate.js
@@ -25,11 +25,20 @@ function digitar() {
         valor_atual = display.textContent
         valor_atual = inverter(valor_atual.toString())
     } else {
-        valor_atual += this.id
+        if (limpar_op) {
+            atualizarDisplay(' ', operacao)
+            limpar_op = false
+        }
+        if (this.id === 'ponto') {
+            if (!(valor_atual.startsWith('0')) && !(valor_atual.includes('.')) && valor_atual.length) {
+                valor_atual += '.'
+            }
+        } else {
+            valor_atual += this.id
+        }
     }
-    atualizarDisplay(Number(valor_atual).toString(), display)
+    if (valor_atual) atualizarDisplay(Number(valor_atual).toString(), display)
 }
-
 function clear() {
     valor_atual = ''
     operacoes = []
