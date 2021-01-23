2f36db33a01d68c5f082838a96b41e7b0132d35b
Denis Lima <denislima99@gmail.com>
2021-01-22 21:05:14 -0300

    Adicionando função de apagar, adicionando função ao botão 'ponto'

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,23 @@
-import { clear, adicionarOperacao, digitar, realizarCalculo } from "./calculate.js";
+import { clear, adicionarOperacao, digitar, pegarResultado, apagar } from "./calculate.js";
 
 const botoes = document.querySelectorAll('button');
 
 function addEvent() {
     for (let botao of botoes) {
-        if (!isNaN(Number(botao.id)) || botao.id === 'inverter') {
+        if (!isNaN(Number(botao.id)) || botao.id === 'inverter' || botao.id === 'ponto') {
             botao.addEventListener('click', digitar)
         }
+
+        if (botao.id === 'apagar') {
+            botao.addEventListener('click', apagar)
+        }
+
         if (botao.id === 'clear') {
             botao.addEventListener('click', clear)
         }
         
         if (botao.id === 'igual') {
-            botao.addEventListener('click', realizarCalculo)
+            botao.addEventListener('click', pegarResultado)
         }
 
         if (botao.getAttribute('class').includes('operator')) {
